28d1468fd71b1036dd18381be66417e23e2fed82
Samuel Almeida <75258537+SamuelAlmeidaS@users.noreply.github.com>
2023-03-20 20:44:32 -0300

    add menu

--- app.js
+++ app.js
@@ -11,42 +11,57 @@ let saida = 0;
 let totalBank = 0;
 let count = 0;
 
+document.querySelector('.balao').addEventListener('click', () => {
+    if (document.querySelector('.navBar').style.display === 'none') {
+        document.querySelector('.navBar').style.display = 'block';
+    } else {
+        document.querySelector('.navBar').style.display = 'none';
+    }
+});
+
 headerButtom.addEventListener('click', () => {
     bgModal.style.display = 'flex';
 });
 
-entradaButtom.addEventListener('click',getValues);
-saidaButtom.addEventListener('click',valueOut);
-document.querySelector('.can__bnt').addEventListener('click', (e)=> {
+entradaButtom.addEventListener('click', getValues);
+
+saidaButtom.addEventListener('click', valueOut);
+
+document.querySelector('.can__bnt').addEventListener('click', (e) => {
     e.preventDefault();
+
     bgModal.style.display = 'none';
-   
 });
 
-function getValues(e){
+function getValues(e) {
     e.preventDefault();
+
     let nameEx = inputName.value;
     entrada += parseFloat(inputValue.value);
-    count +=  parseFloat(inputValue.value);
+    count += parseFloat(inputValue.value);
     totalBank = count;
-    document.querySelector('.total').innerText = `R$ ${totalBank.toFixed(2).replace('.',',')}`;
-    document.querySelector('.saldo__p').innerText = `R$ ${entrada.toFixed(2).replace('.',',')}`;
-    extrato(nameEx,entrada,'green');
+
+    document.querySelector('.total').innerText = `R$ ${totalBank.toFixed(2).replace('.', ',')}`;
+    document.querySelector('.saldo__p').innerText = `R$ ${entrada.toFixed(2).replace('.', ',')}`;
+
+    extrato(nameEx, entrada, 'green');
 }
 
-function valueOut(e){
+function valueOut(e) {
     e.preventDefault();
+
     let nameEx = inputName.value;
     saida += parseFloat(inputValue.value);
-    count =  parseFloat(totalBank) - saida;
+    count = parseFloat(totalBank) - saida;
     totalBank = count;
-    document.querySelector('.total').innerText = `R$ ${totalBank.toFixed(2).replace('.',',')}`;
-    document.querySelector('.saldo__n').innerText = `R$ ${saida.toFixed(2).replace('.',',')}`;
 
-    extrato(nameEx,saida, 'red');
+    document.querySelector('.total').innerText = `R$ ${totalBank.toFixed(2).replace('.', ',')}`;
+    document.querySelector('.saldo__n').innerText = `R$ ${saida.toFixed(2).replace('.', ',')}`;
+
+    extrato(nameEx, saida, 'red');
 }
 
-function extrato(name, value,color) {
+function extrato(name, value, color) {
     let li = document.createElement('li');
     let divName = document.createElement('div');
     let divValue = document.createElement('div');

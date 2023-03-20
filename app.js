1961021ba4e72664cc05c875d12f959c79cf3ea3
Samuel Almeida <75258537+SamuelAlmeidaS@users.noreply.github.com>
2023-03-20 18:32:36 -0300

    second commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const headerButtom = document.querySelector('.headerButtom');
+const bgModal = document.querySelector('.bg-modal');
+headerButtom.addEventListener('click', () => {
+    bgModal.style.display = 'flex';
+});
